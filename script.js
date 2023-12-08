9006dade08a8b2899e0028bff2eeaaa72b50c1f7
walkerbuddy01 <136956295+walkerbuddy01@users.noreply.github.com>
2023-12-08 09:41:53 +0530

    changes on the hero page animation

--- script.js
+++ script.js
@@ -1,45 +1,48 @@
-// for the smooth scrolling locomotive js 
+// for the smooth scrolling locomotive js
 
-function locomotiveScroll(){
+function locomotiveScroll() {
   gsap.registerPlugin(ScrollTrigger);
 
-// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
-const locoScroll = new LocomotiveScroll({
-  el: document.querySelector(".wrapper"),
-  smooth: true
-});
-// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-locoScroll.on("scroll", ScrollTrigger.update);
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector(".wrapper"),
+    smooth: true,
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
 
-// tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
-ScrollTrigger.scrollerProxy(".wrapper", {
-  scrollTop(value) {
-    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-  getBoundingClientRect() {
-    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-  },
-  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-  pinType: document.querySelector(".wrapper").style.transform ? "transform" : "fixed"
-});
+  // tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy(".wrapper", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector(".wrapper").style.transform
+      ? "transform"
+      : "fixed",
+  });
 
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
-// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-ScrollTrigger.refresh();
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
 }
 
 locomotiveScroll();
 
-
-
-
-
-
-// cursor follower with effect 
+// cursor follower with effect
 
 function cursorFollower() {
   var mouseFollower = document.querySelector(".mouse-follower");
@@ -53,25 +56,39 @@ function cursorFollower() {
   wrapper.addEventListener("mouseenter", function () {
     gsap.to(".mouse-follower", {
       scale: 2,
-      opacity:1,
+      opacity: 1,
     });
   });
   wrapper.addEventListener("mouseleave", function () {
     gsap.to(".mouse-follower", {
       scale: 0,
-      opacity:0,
+      opacity: 0,
     });
   });
 }
 cursorFollower();
 
+function wordAnimation() {
+  gsap.to(".W1", {
+    y: 0,
+    duration: 1,
+    ease: 'power2.out',
+    delay:0.25,
+    stagger: 0.2
+  });
+
+  gsap.to(".W2", {
+    y: 0,
+    duration: 1,
+    ease: 'power2.out',
+    delay:0.25,
+    stagger: 0.2,
+  });
+}
+
+wordAnimation();
 
 
 
-gsap.from(".wrd-setting",{
-  scale:0,
-  stagger:0.25,
-  duration:1,
-  scrub:2,
-})
+
 
